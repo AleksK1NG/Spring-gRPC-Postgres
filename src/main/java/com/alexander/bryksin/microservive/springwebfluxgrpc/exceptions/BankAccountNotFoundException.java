e7d43adba782d9e440872b2f95ec330e93657590
Alexander <alexander.bryksin@yandex.ru>
2022-11-20 17:33:15 +0300

    feature: add bank account http controller advice

--- src/main/java/com/alexander/bryksin/microservive/springwebfluxgrpc/exceptions/BankAccountNotFoundException.java
+++ src/main/java/com/alexander/bryksin/microservive/springwebfluxgrpc/exceptions/BankAccountNotFoundException.java
@@ -2,10 +2,10 @@ package com.alexander.bryksin.microservive.springwebfluxgrpc.exceptions;
 
 public class BankAccountNotFoundException extends RuntimeException {
     public BankAccountNotFoundException(String id) {
-        super(String.format("bank account with %s not found", id));
+        super(String.format("bank account with id %s not found", id));
     }
 
     public BankAccountNotFoundException(String id, Throwable cause) {
-        super(String.format("bank account with %s not found", id), cause);
+        super(String.format("bank account with id %s not found", id), cause);
     }
 }
